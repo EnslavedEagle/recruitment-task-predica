b2322a3cd0204303c1bebe15f74d72654c393a9e
Patryk Bernasiewicz <kontakt@patrykb.pl>
2018-02-08 13:17:52 +0100

    added Categories component and Interfaces: Category, Link

---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Category, Link } from '../../../interfaces';
+
+@Component({
+  selector: 'pre-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.sass']
+})
+export class CategoriesComponent {
+  @Input() public categories: Category;
+  @Input() public search: string;
+
+  constructor() {}
+}
---
+++ src/interfaces/category.ts
@@ -0,0 +1,6 @@
+import { Link } from '.';
+
+export interface Category {
+  name: string;
+  links: Link[];
+}
